d6a0791d824a3cac8a67198c5db69e029e10c7bf
Gustavo Falavigna <gfalav@hotmail.com>
2018-11-07 12:06:03 -0300

    company and userdetails design added

--- Company-userdetails.bat
+++ Company-userdetails.bat
@@ -1,2 +1,3 @@
 rails g scaffold Company name:string website:string email:string industry:string phone:string address:text zipcode:string country:integer profile:text emailmng:string
-rails g scaffold Userdetail user:references name:string company:references phone:string address:string usrphoto:string
+rails g scaffold Team name:string department:string company:references owner:integer
+rails g scaffold Userdetail user:references team:references name:string phone:string address:string usrphoto:string
