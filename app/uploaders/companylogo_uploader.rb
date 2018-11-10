2a150cae5d78466a80cb37357e33999234a6d9b6
Gustavo Falavigna <gfalav@hotmail.com>
2018-11-10 19:12:48 -0300

    companies addedd

--- app/uploaders/companylogo_uploader.rb
+++ app/uploaders/companylogo_uploader.rb
@@ -22,16 +22,16 @@ class CompanylogoUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [200, 300]
+  process resize_to_limit: [300, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_limit: [40, 40]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
