10109c7e32a62b494379364452477eeff3ca2300
Gustavo Falavigna <gfalav@hotmail.com>
2018-11-17 14:43:27 -0300

    arr user_id on userdetails

--- app/controllers/userdetails_controller.rb
+++ app/controllers/userdetails_controller.rb
@@ -64,7 +64,11 @@ class UserdetailsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_userdetail
-      @userdetail = Userdetail.find(params[:id])
+      if Userdetail.where(:id=> params[:id]).count == 0
+        @userdetail = Userdetail.new
+      else
+        @userdetail = Userdetail.find(params[:id])
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
